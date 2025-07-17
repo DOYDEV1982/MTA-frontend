f92e860364566342d93f47314932a09337fdf3ca
DOYDEV1982 <oladoyejelili53@yahoo.com>
2025-07-17 11:59:20 +0100

    JS ASSIGNMENT

---
+++ script.js
@@ -0,0 +1,206 @@
+function check(digit){
+    if(digit >0){
+        return "positive"
+    }else if(digit ==0){
+        return "zero"
+    }else if(digit <=0){
+        return "negative"
+    }else if(digit >0 ==0 <0){
+        return "prints"
+    }
+}
+undefined
+check(1)
+'positive'
+check(0)
+'zero'
+check(-1)
+'negative'
+check(">0 ==0 <0"){
+'prints'
+
+}
+function check(weather){
+    if(weather == "rainy"){
+        return "umbrella"
+    }else if(weather == "sunny"){
+        return "sunscreen"
+    }else if(weather == "cold"){
+        return "warm"
+    }else if(weather == "umbrella sunny cold"){
+        return "prints"
+    }
+}
+
+undefined
+check("rainy")
+'umbrella'
+check("sunny")
+'sunscreen'
+check("cold")
+'warm'
+check("umbrella sunny cold"){
+'prints'
+}
+
+function check(age){
+    if(age <13){
+        return "Child"
+    }else if(age <19){
+        return "Teenager"
+    }else if(age <64){
+        return "Adult"
+    }else if(age >=65){
+        return "Senior"
+    }else if("<13 >=13 <64 >=65"){
+        return "prints"
+    }
+}
+undefined
+check(12)
+'Child'
+check(16)
+'Teenager'
+check(45)
+'Adult'
+check(85)
+'Senior'
+check("<13 >=13 <64 >=65"){
+'prints'
+
+}
+
+
+function check(day){
+    if(day == "saturday"){
+        return "weekend"
+    }else if( day == "thursday"){
+        return "weekday"
+    }else if( day == "saturday thursday"){
+        return "prints"
+    }
+}
+undefined
+check("saturday")
+'weekend'
+check("thursday")
+'weekday'
+check("saturday thursday"){
+'prints'
+
+}
+
+
+function check(light){
+    if(light == "green"){
+        return "Go"
+    }else if(light == "yellow"){
+        return "Slow down"
+    }else if(light == "red"){
+        return "Stop"
+    }else if(light == "green yellow red"){
+        return "prints"
+    }
+}
+undefined
+check("green")
+'Go'
+check("yellow")
+'Slow down'
+check("red")
+'Stop'
+check("green yellow red"){
+'prints'
+
+}
+
+
+function check(role){
+    if(role == "cordinate"){
+        return "Welcome Admin"
+    }else if(role == "edit"){
+        return "Welcome Editor"
+    }else if(role == "observer"){
+        return "Welcome Viewer"
+    }else if(role == "anything again"){
+        return "Not recognized"
+    }else if(role == "cordinate edit observer anything again"){
+        return "prints"
+    }
+}
+undefined
+check("cordinate")
+'Welcome Admin'
+check("edit")
+'Welcome Editor'
+check("observer")
+'Welcome Viewer'
+check("anything again")
+'Not recognized'
+check("cordinate edit observer anything again"){
+'prints'
+
+}
+
+
+function check(grade){
+    if(grade >90){
+        return "Excellent"
+    }else if(grade >=70 && 89){
+        return "Good"
+    }else if(grade >=50 && 69){
+        return "Pass"
+    }else if(grade <50){
+        return "Fail"
+    }else if(grade >90 >=70 && 89 >=50 && 69 <50){
+        return "Prints"
+    }
+}
+undefined
+check(95)
+'Excellent'
+check(88)
+'Good'
+check(68)
+'Pass'
+check(49)
+'Fail'
+
+function check(digit){
+    if(digit <12){
+        return "Good morning"
+    }else if(digit <=17){
+        return "Good afternoon"
+    }else if(digit >17){
+        return "Good evening"
+    }else if(digit <12 <=17 >17){
+        return "prints"
+    }
+}
+undefined
+check(11)
+'Good morning'
+check(16)
+'Good afternoon'
+check(20)
+'Good evening'
+
+
+function check(attendant){
+    if(attendant ==100){
+        return "Perfect Attendance"
+    }else if(attendant >=75){
+        return "Good Attendance"
+    }else if(attendant <75){
+        return "Low Attandance"
+    }else if(attendant ==100 >=75 <75){
+        return "prints"
+    }
+}
+undefined
+check(100)
+'Perfect Attendance'
+check(89)
+'Good Attendance'
+check(70)
+'Low Attandance'
