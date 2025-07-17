37ec236f86ceda8eeb337c6e325a364b5674a026
DOYDEV1982 <oladoyejelili53@yahoo.com>
2025-07-17 12:35:50 +0100

    JavaScript

---
+++ Assignment__js/main.js
@@ -0,0 +1,426 @@
+let user = {
+    firstname: "ola",
+    lastname: "ade",
+    age: "30",
+    email: "ade@.com",
+    pwd: "1111",
+    confirmpwd: "1111",
+}
+undefined
+user.firstname
+'ola'
+user.lastname
+'ade'
+user.email
+'ade@.com'
+user.pwd
+'1111'
+user.confirmpwd
+'1111'
+user
+{firstname: 'ola', lastname: 'ade', age: '30', email: 'ade@.com', pwd: '1111', …}
+let cars ="number"
+undefined
+1
+1
+let 1 = "1, 2, 3, 4, 5, 6"
+VM662:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+let number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+undefined
+2
+2
+4
+4
+6
+6
+6
+6
+.pop 20
+VM732:1 Uncaught SyntaxError: Unexpected token '.'Understand this error
+.pop 4
+VM764:1 Uncaught SyntaxError: Unexpected token '.'Understand this error
+.shift 1, 2
+VM799:1 Uncaught SyntaxError: Unexpected token '.'Understand this error
+let number=["2,4,8,10,12,16]
+VM854:1 Uncaught SyntaxError: Invalid or unexpected tokenUnderstand this error
+let come = ["a", "b", "c", "d", "e", "f"]
+undefined
+let come.push (doydev)
+VM1148:1 Uncaught SyntaxError: Unexpected token '.'Understand this error
+come.push (doydev)
+VM1156:1 Uncaught ReferenceError: doydev is not defined
+    at <anonymous>:1:12
+(anonymous) @ VM1156:1Understand this error
+come.push ("doydev")
+7
+come
+(7) ['a', 'b', 'c', 'd', 'e', 'f', 'doydev']
+come.pop 
+ƒ pop() { [native code] }
+come.pop ("f")
+'doydev'
+come
+(6) ['a', 'b', 'c', 'd', 'e', 'f']
+function welcome (){
+    return 13-5
+    
+}
+undefined
+function 
+
+    9
+function welcome (){
+    return 13+5
+}
+VM1416:3 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function 
+
+    9
+function welcome (){
+    return 13+
+}
+function
+VM1456:3 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function 
+
+    9
+function welcome (){
+    return 13+5
+}
+function
+VM1459:3 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function 
+
+    9
+function welcome (){
+    return 13+5
+}
+function
+VM1510:3 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function 
+
+    9
+function welcome (){
+    return 13+5
+}
+welcome
+VM1598:3 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function adeola(){
+    return 13-5
+}
+
+undefined
+adeola
+ƒ adeola(){
+    return 13-5
+}
+function adeola(2, 3,){}
+        return (2*3)
+VM1900:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function adeola(2, 3,){
+        return (2*3)
+}
+VM1909:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function adeola(2, 3,){
+        return (2*3)
+}
+
+VM1923:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function adeola(x, y){
+        return x*y
+}
+
+undefined
+adeola(2,3)
+6
+adeola(5,8)
+40
+adeola()
+NaN
+"welcome
+VM2077:1 Uncaught SyntaxError: Invalid or unexpected tokenUnderstand this error
+"welcome" + "back"
+'welcomeback'
+function sayhello (name){
+    return "hello" + name + "!!"
+}
+undefined
+sayhello
+ƒ sayhello (name){
+    return "hello" + name + "!!"
+}
+sayhello("kay")
+'hellokay!!'
+const newsayhello = (p1 , p2) => {
+    return p1 * p2
+}
+undefined
+newsayhello(20, 30)
+600
+newsayhello(20, 30)
+600
+const newsayhello = (p1 , p2) return p1 + p2
+VM2575:1 Uncaught SyntaxError: Unexpected token 'return'Understand this error
+// > < => <= ll
+undefined
+// > < => <= ||
+undefined
+true || fasle
+true
+true || true
+true
+fasle 
+VM2812:1 Uncaught ReferenceError: fasle is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM2812:1Understand this error
+fasle || fasle
+VM2846:1 Uncaught ReferenceError: fasle is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM2846:1Understand this error
+false || false
+false
+true && false
+false
+true && true
+true
+if(true || true){}
+undefined
+if(true || true){}
+undefined
+function agecondition(age){
+    if(age => 18){
+        return "you are older"        
+}else if(age =< 12){
+        return "too small"
+        
+VM3336:4 Uncaught SyntaxError: Unexpected token '<'Understand this error
+function namecat(params){
+    if(par > 18){
+        return "18"
+    }else if(par <= 12){
+        return "12"
+    }else if(par > 100){
+    }else{
+          return 
+}
+    
+VM3586:10 Uncaught SyntaxError: Unexpected end of inputUnderstand this error
+function namecat(params){
+    if(par > 18){
+        return "18"
+    }else if(par <= 12){
+        return "12"
+    }else if(par > 100){
+    }else{
+    }     return "sorry what ur age"
+}
+    
+undefined
+namecat
+ƒ namecat(params){
+    if(par > 18){
+        return "18"
+    }else if(par <= 12){
+        return "12"
+    }else if(par > 100){
+    }else{
+    }     return "sorry what ur age"
+}
+function namecat(params){
+    if(par > 18){
+        return "18"
+    }else if(par <= 12){
+        return "12"
+    }else if(par > 100){
+        return  "110"
+    }else{
+    }     return "sorry what ur age"
+  }
+
+    
+undefined
+namecat80
+VM3733:1 Uncaught ReferenceError: namecat80 is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM3733:1Understand this error
+namecat100
+VM3748:1 Uncaught ReferenceError: namecat100 is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM3748:1Understand this error
+namecat110
+VM3766:1 Uncaught ReferenceError: namecat110 is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM3766:1Understand this error
+function nameAssign(70){
+        if
+VM3868:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function nameAssign(70){
+        if(70 >){
+        return "A"
+ } 
+VM4004:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function nameAssign(70){
+        if(70 >){
+        return "A"
+ } 
+VM4050:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function nameAssign(70){
+        if(70 > 100){
+        return "A"
+ } 
+VM4056:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function nameAssign(70){
+        if(100 > 70){
+        return "A"
+ } 
+VM4066:1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+function nameAssign(doy){
+        if(doy >69 && doy <101){
+          return "A"
+    }else if(doy >49 && doy <70){
+          return "B"
+    }else if(doy >29 && doy <50 ){
+          return "C"
+    }else if(doy >19  && doy <30){
+          return  "D"
+    }else if(doy >9  && doy  <20){
+            return "E"
+    }         
+        
+ } 
+undefined
+function doystore(product){
+    if(product == "a"){
+        return "a"
+        
+    }
+}
+undefined
+function doystore(product){
+    if(product == "a"){
+        return "a"
+    }else if(product == "b"){
+        return "b"
+    }else if(product == "1"){
+        return "value not find"){
+        
+    }
+}
+VM4875:7 Uncaught SyntaxError: Unexpected token ')'Understand this error
+function doystore(product){
+    if(product == "a"){
+        return "a"
+    }else if(product == "b"){
+        return "b"
+    }else if(product == "1"){
+        return "value not find"
+        
+    }
+}
+undefined
+doystore("a")
+'a'
+doystore("b")
+'b'
+doystore("1")
+'value not find'
+doystore("1")
+'value not find'
+function devstore(letter){
+    if(letter== "a"){
+        return "rice"
+    }
+    else if(letter== "b"){
+         return "beans"
+    }
+}
+undefined
+function devstore(letter){
+    if(letter== "a"){
+        return "rice"
+    }
+    else if(letter== "b"){
+         return "beans"
+    }
+
+else if(letter== "c"){
+         return "cassava"
+    }
+else if(letter== "d"){
+         return "garri"
+    }
+else if(letter== "e"){
+         return "yam"
+    }
+else if(letter== "f"){
+         return "goat"
+    }
+else if(letter== "g"){
+         return "isu"
+    }
+else if(letter== "h"){
+         return "apple"
+    }
+else if(letter== "i"){
+         return "amala"
+    }
+else if(letter== "j"){
+         return "net"
+    }
+else if(letter== "k"){
+         return "mango"
+    }
+else if(letter== "l"){
+         return "kettle"
+    }
+else if(letter== "m"){
+         return "indomine"
+    }
+else if(letter== "n"){
+         return "fish"
+    }
+else if(letter== "o"){
+         return "egg"
+    }
+else if(letter== "p"){
+         return "dog"
+    }
+else if(letter== "q"){
+         return "cat"
+    }
+else if(letter== "r"){
+         return "rat"
+    }
+else if(letter== "s"){
+         return "melon"
+    }
+else if(letter== "t"){
+         return "pawpaw"
+    }
+else if(letter== "u"){
+         return "ekusi"
+    }
+else if(letter== "v"){
+         return "alubose"
+    }
+else if(letter== "w"){
+         return "onion"
+    }
+else if(letter== "x"){
+         return "ila"
+    }
+else if(letter== "y"){
+         return "iyan"
+    }
+else if(letter== "z"){
+         return "fufu"
+    }
+else{
+     return "value not find"
+    }
+}
+undefined
+devstore"z"
+VM5699:1 Uncaught SyntaxError: Unexpected stringUnderstand this error
+devstore("z")
+'fufu'
